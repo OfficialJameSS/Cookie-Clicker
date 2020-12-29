ffb54606cbc3221a3c44abb3c7a495aa951d7a9b
JacobeeQ <73647980+JacobeeQ@users.noreply.github.com>
2020-12-29 15:04:14 +0100

    Cookie Clicker files!
    This is Cookie Clicker, i created in Visual Studio.

---
+++ Cookie Clicker/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Cookie_Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CPS: ";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.Yellow;
+            this.button1.Location = new System.Drawing.Point(168, 407);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 69);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "CLICK!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(427, 492);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Gold;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Best score CPS: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(14, 459);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Rank: ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 488);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Cookie Clicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Cookie Clicker/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using WMPLib;
+
+namespace Cookie_Clicker
+{
+    public partial class Form1 : Form
+    {
+        Timer t = new Timer();
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
+        int score, bestScore;
+        string rank = "NOOB";
+
+        public Form1()
+        {
+            player.URL = "button_click.mp3";
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Witaj w Cookie Clickerze!", "Cookie Clicker");
+            t.Start();
+            label1.Text = "CPS: " + Convert.ToString(score);
+            label3.ForeColor = Color.White;
+            label3.Text = "Rank: " + rank;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            player.controls.play();
+            score++;
+            label1.Text = "CPS: " + Convert.ToString(score);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your rank: " + rank);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (score > bestScore)
+            {
+                bestScore = score;
+                label2.Text = "Best score CPS: " + bestScore + " CPS";
+            }
+
+
+            if (bestScore >= 3 && bestScore < 6)
+            {
+                rank = "Snail";
+                label3.ForeColor = Color.Green;
+                label3.Text = "Rank: " + rank;
+            }
+            else if (bestScore >= 6 && bestScore < 10)
+            {
+                rank = "Cookie";
+                label3.ForeColor = Color.Brown;
+                label3.Text = "Rank: " + rank;
+            }
+            else if (bestScore >= 10 && bestScore < 15)
+            {
+                rank = "Speedy";
+                label3.ForeColor = Color.Yellow;
+                label3.Text = "Rank: " + rank;
+            }
+            else if (bestScore >= 15 && bestScore < 20)
+            {
+                rank = "Master";
+                label3.ForeColor = Color.Blue;
+                label3.Text = "Rank: " + rank;
+            }
+            else if (bestScore >= 20 && bestScore < 25)
+            {
+                rank = "Legend";
+                label3.ForeColor = Color.Red;
+                label3.Text = "Rank: " + rank;
+            }
+            else if (bestScore >= 25)
+            {
+                rank = "God";
+                label3.ForeColor = Color.Magenta;
+                label3.Text = "Rank: " + rank;
+            }
+
+            score = 0;
+            label1.Text = "CPS: " + Convert.ToString(score);
+        }
+    }
+}
